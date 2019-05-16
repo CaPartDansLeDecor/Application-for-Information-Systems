af1fff24271e7588904fa785a5c81d1cd3020945
CaPartDansLeDecor <45480583+CaPartDansLeDecor@users.noreply.github.com>
2019-05-16 10:19:04 +0200

    Add files via upload

---
+++ B3251_POSITIF-2/src/main/java/Serialisation/SerialisationGenererPredictions.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Serialisation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import metier.data.Voyance;
+
+/**
+ *
+ * @author mguilhin
+ */
+public class SerialisationGenererPredictions extends Serialisation{
+    
+    @Override
+    public void serialiser(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+        JsonObject jsonContainer = new JsonObject();
+        
+        Boolean ans = (Boolean) request.getAttribute("voyanceEnCours");
+        jsonContainer.addProperty("voyanceEnCours",ans);
+        if(ans){
+            jsonContainer.addProperty("coeur", (String) request.getAttribute("coeur"));
+            jsonContainer.addProperty("sante", (String) request.getAttribute("sante"));
+            jsonContainer.addProperty("travail", (String) request.getAttribute("travail"));
+        }
+        PrintWriter out = this.getWriterWithJsonHeader(response);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        gson.toJson(jsonContainer, out);
+        
+    }
+    
+}
