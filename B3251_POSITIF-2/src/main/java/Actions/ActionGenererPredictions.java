166ec6b21e5710a135e9ae8781cf81cab0287ae2
CaPartDansLeDecor <45480583+CaPartDansLeDecor@users.noreply.github.com>
2019-05-16 10:18:41 +0200

    Add files via upload

---
+++ B3251_POSITIF-2/src/main/java/Actions/ActionGenererPredictions.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Actions;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import metier.data.Client;
+import metier.data.Employe;
+import metier.data.Voyance;
+import metier.service.Service;
+
+/**
+ *
+ * @author mguilhin
+ */
+public class ActionGenererPredictions extends Action{
+    
+    @Override
+    public boolean executer(HttpServletRequest request){
+        
+        Service service = new Service();
+        System.out.println("gne");
+        String coeur = request.getParameter("coeur");
+        String sante = request.getParameter("sante");
+        String travail = request.getParameter("travail");
+        
+        //On récupère le client
+        
+        HttpSession session = request.getSession(true);
+        Employe employeConnecte = (Employe)session.getAttribute("Connected");
+        
+        List<Voyance> voyances = employeConnecte.getListeVoyance();
+        Voyance voyance = null;
+        for(Voyance v : voyances){
+            if(v.getDebut()!=null && v.getFin()==null){
+                voyance = v;
+            }
+        }
+        //System.out.println(voyance);
+        if(voyance == null){
+            request.setAttribute("voyanceEnCours",false);
+        } else {
+            request.setAttribute("voyanceEnCours",true);
+            Client c = voyance.getClient();
+            List<String> listePredictions = service.genererPredictions(c,Integer.parseInt(coeur),Integer.parseInt(sante),Integer.parseInt(travail));
+            request.setAttribute("coeur",listePredictions.get(0));
+            request.setAttribute("sante",listePredictions.get(1));
+            request.setAttribute("travail",listePredictions.get(2));
+        }
+        return false;
+    }
+    
+}
